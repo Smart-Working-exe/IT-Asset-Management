71b121c29ef471658060e2d554fb9c5e02108e7b
Julez <programming@grillenmeier.net>
2022-11-05 17:17:09 +0100

    Update Dashboard und Login

---
+++ html/dashboard_admin.php
@@ -0,0 +1,71 @@
+<html lang="de">
+<head>
+    <meta charset="UTF-8" http-equiv="X-UA-Compatible" content="IE=edge" name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles/style.css">
+    <title>Dashboard</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <img class="col-lg-3 img-fluid" src="/img/fh-aachen_university-of-applied-sciences_303_logo.png" alt="fhlogo">
+        <div class="col-lg-6"><p class="h1 text-center mt-4"> IT Asset Management</p></div>
+        <div class="col-lg-3">
+            <form method="get">
+                <button type="submit" class="btn btn-danger mt-4">Abmelden</button>
+            </form>
+        </div>
+    </div>
+    <div class="row mt-5 row justify-content-between">
+        <div class="btn-group-vertical col-lg-5 mt-3 offset-1">
+            <button type="button" class="btn btn-primary sub">Raumansicht</button>
+            <button type="button" class="btn btn-primary sub mt-2">Datenbank</button>
+            <button type="button" class="btn btn-primary sub mt-2">Ausleihe</button>
+            <button type="button" class="btn btn-primary sub mt-2">Gerät hinzufügen</button>
+            <button type="button" class="btn btn-primary sub mt-2">Person hinzufügen</button>
+            <button type="button" class="btn btn-primary sub mt-2">Softwarelizenzen</button>
+            <button type="button" class="btn btn-primary sub mt-2">Systemlogs</button>
+            <button type="button" class="btn btn-primary sub mt-2">Einstellungen</button>
+        </div>
+        <div class="col-lg-5">
+            <div class="row">
+                <p class="display-6 h6 text-center col-lg-4 mt-3"> Benachrichtungen</p>
+            </div>
+
+            <div class="toast show col-lg-6 mt-2">
+                <div class="toast-header ">
+                    Important
+                    <button type="button" class="btn-close" data-bs-dismiss="toast"></button>
+                </div>
+                <div class="toast-body">
+                    Die Lizenz von Microsoft Visual Studio läuft auf PC1 in 3 Tagen ab.
+                </div>
+            </div>
+
+            <div class="toast show col-lg-6 mt-2">
+                <div class="toast-header">
+                    Important
+                    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                </div>
+                <div class="toast-body">
+                    Die Lizenz von MS Office läuft auf PC2 in 14 Tagen ab.
+                </div>
+            </div>
+
+            <div class="toast show col-lg-6 mt-2">
+                <div class="toast-header ">
+                    Important
+                    <button type="button" class="btn-close" data-bs-dismiss="toast"></button>
+                </div>
+                <div class="toast-body">
+                    In Raum A001 sind 30 von 32 IP-Adressen belegt.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
+</html>
--- html/login.php
+++ html/login.php
@@ -2,13 +2,12 @@
 
 <!--
  - SmartWorking.exe
- - Author: Julian Grillenmeier
 -->
 
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/styles/style.css">
     <style>
         body {
             font: 14px sans-serif;
@@ -21,29 +20,40 @@
     </style>
 </head>
 <body>
-<div id="login">
-    <img src="/img/fh-aachen_university-of-applied-sciences_303_logo.png" alt="FH Aachen Logo">
-    <h1>Login</h1>
+<div class="container justify-content-center align-items-center" id="login">
+    <div class="row mt-5 justify-content-center">
+        <img class="img-fluid" src="/img/fh-aachen_university-of-applied-sciences_303_logo.png" alt="FH Aachen Logo">
+    </div>
+    <div class="row justify-content-center">
+        <p class=" text-center display-3 login" >Login</p></div>
+    </div>
 
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <div class="form-group">
-            <label>Benutzername</label>
-            <input type="text" name="username"
-                   class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>"
-                   placeholder="Benutzername">
-            <span class="invalid-feedback"><?php echo $username_err; ?></span>
-        </div>
-        <div class="form-group">
-            <label>Passwort</label>
-            <input type="password" name="password"
-                   class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>"
-                   placeholder="Passwort">
-            <span class="invalid-feedback"><?php echo $password_err; ?></span>
-        </div>
-        <div class="form-group">
-            <input type="submit" class="btn btn-primary" value="Login">
-        </div>
-    </form>
+    <div class="row mt-3 justify-content-center align-items-center">        
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group mb-3 mt-3">
+                <label for="username">Benutzername*</label>
+                <input type="text" name="username" id="username"
+                       class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>"
+                       placeholder="Benutzername">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>
+            <div class="form-group mb-3">
+                <label for="pwd">Passwort*</label>
+                <input type="password" name="password" id="pwd"
+                       class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>"
+                       placeholder="Passwort">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-check mb-3">
+                <label class="form-check-label">
+                    <input class="form-check-input" type="checkbox" name="remember"> Remember me
+                </label>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary sub" value="Login">
+            </div>
+        </form>
+    </div>
 </div>
 </body>
 </html>
