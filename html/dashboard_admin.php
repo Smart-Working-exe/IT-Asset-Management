be598b2399e8a8c00aa5c1010b260d415d0dae0b
Julez <programming@grillenmeier.net>
2022-11-05 17:23:24 +0100

    CSS Import

--- html/dashboard_admin.php
+++ html/dashboard_admin.php
@@ -2,7 +2,9 @@
 <head>
     <meta charset="UTF-8" http-equiv="X-UA-Compatible" content="IE=edge" name="viewport"
           content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/styles/style.css">
+    <!-- CSS only --><!-- CSS only -->
+    <link rel="stylesheet" href="/styles/bootstrap.css">
+    <link rel="stylesheet" href="/styles/custom.css">
     <title>Dashboard</title>
 </head>
 <body>
