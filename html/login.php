beee73164f1601d521026ee6430ee3225f36d2fe
Julez <programming@grillenmeier.net>
2022-11-05 12:34:36 +0100

    LoginPage_Hardcode
    Erste schritte der Login Page

---
+++ html/login.php
@@ -0,0 +1,50 @@
+<!doctype html>
+
+<!--
+ - SmartWorking.exe
+ - Author: Julian Grillenmeier
+-->
+
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body {
+            font: 14px sans-serif;
+        }
+
+        .login {
+            width: 360px;
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+<div id="login">
+    <img src="/img/fh-aachen_university-of-applied-sciences_303_logo.png" alt="FH Aachen Logo">
+    <h2>Login</h2>
+    <p>Please fill in your credentials to login.</p>
+
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div class="form-group">
+            <label>Benutzername</label>
+            <input type="text" name="username"
+                   class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>"
+                   placeholder="Benutzername">
+            <span class="invalid-feedback"><?php echo $username_err; ?></span>
+        </div>
+        <div class="form-group">
+            <label>Passwort</label>
+            <input type="password" name="password"
+                   class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>"
+                   placeholder="Passwort">
+            <span class="invalid-feedback"><?php echo $password_err; ?></span>
+        </div>
+        <div class="form-group">
+            <input type="submit" class="btn btn-primary" value="Login">
+        </div>
+    </form>
+</div>
+</body>
+</html>
