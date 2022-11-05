00482b29dbd4014c27cd51cea38737fad50292e9
Julez <programming@grillenmeier.net>
2022-11-05 17:48:47 +0100

    Update Login
    Login update,  Bildgröße muss noch angepasst werden.

--- html/login.php
+++ html/login.php
@@ -6,18 +6,11 @@
 
 <html lang="de">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8" http-equiv="X-UA-Compatible" content="IE=edge" name="viewport"
+          content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/styles/bootstrap.css">
     <link rel="stylesheet" href="/styles/custom.css">
     <style>
-        body {
-            font: 14px sans-serif;
-        }
-
-        .login {
-            width: 360px;
-            padding: 20px;
-        }
     </style>
 </head>
 <body>
@@ -26,21 +19,21 @@
         <img class="img-fluid" src="/img/fh-aachen_university-of-applied-sciences_303_logo.png" alt="FH Aachen Logo">
     </div>
     <div class="row justify-content-center">
-        <p class=" text-center display-3 login" >Login</p></div>
+        <div class="col-lg-4 "><p class=" text-center display-3 login">Login</p></div>
     </div>
 
-    <div class="row mt-3 justify-content-center align-items-center">        
+    <div class="row mt-3 justify-content-center align-items-center">
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group mb-3 mt-3">
                 <label for="username">Benutzername*</label>
-                <input type="text" name="username" id="username"
+                <input type="text" name="username" id="username" required="true"
                        class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>"
                        placeholder="Benutzername">
                 <span class="invalid-feedback"><?php echo $username_err; ?></span>
             </div>
             <div class="form-group mb-3">
                 <label for="pwd">Passwort*</label>
-                <input type="password" name="password" id="pwd"
+                <input type="password" name="password" id="pwd" required="true"
                        class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>"
                        placeholder="Passwort">
                 <span class="invalid-feedback"><?php echo $password_err; ?></span>
@@ -56,5 +49,6 @@
         </form>
     </div>
 </div>
+<script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
