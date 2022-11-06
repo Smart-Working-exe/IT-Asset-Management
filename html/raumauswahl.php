610903d75d3eecde6df3b0262d7688086347db4d
mohamedzarrouk07 <mohamedaziz.zarrouk07@gmail.com>
2022-11-06 14:17:21 +0100

    Adds Raumauswahl
    Raumauswahl hier fertig,
    UX Fixes später

--- html/raumauswahl.php
+++ html/raumauswahl.php
@@ -19,21 +19,151 @@
             </form>
         </div>
     </div>
-    <div class="row p-5">
-        <div class="col-lg-6 ">
-            <div class="dropdown ">
-                <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
-                    Dropdown button
-                </button>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Link 1</a></li>
-                    <li><a class="dropdown-item" href="#">Link 2</a></li>
-                    <li><a class="dropdown-item" href="#">Link 3</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
+    <div class="row p-4 mt-4">
+        <img src="../img/Lageplan.png" alt="lagepln" class="col-lg-8" style="height: 60vh;">
+        <div id="accordion " class="col-lg-4">
 
+            <div class="card">
+                <div class="card-header">
+                    <a class="btn" data-bs-toggle="collapse" href="#collapseOne">
+                        Gebäude A
+                    </a>
+                </div>
+                <div id="collapseOne" class="collapse show" data-bs-parent="#accordion">
+                    <div class="card-body">
+                        <a href="#"> Raum A001</a>
+                        <a href="#"> Raum A002</a>
+                        <a href="#"> Raum A003</a>
+                        <a href="#"> Raum A004</a>
+                        <a href="#"> Raum A101</a>
+                        <a href="#"> Raum A102</a>
+                        <a href="#"> Raum A103</a>
+                        <a href="#"> Raum A104</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    <a class="collapsed btn" data-bs-toggle="collapse" href="#collapseTwo">
+                        Gebäude B
+                    </a>
+                </div>
+                <div id="collapseTwo" class="collapse" data-bs-parent="#accordion">
+                    <div class="card-body">
+                        Lorem ipsum..
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    <a class="collapsed btn" data-bs-toggle="collapse" href="#collapseThree">
+                        Gebäude C
+                    </a>
+                </div>
+                <div id="collapseThree" class="collapse" data-bs-parent="#accordion">
+                    <div class="card-body">
+                        Lorem ipsum..
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    <a class="btn" data-bs-toggle="collapse" href="#collapseFour">
+                        Gebäude D
+                    </a>
+                </div>
+                <div id="collapseFour" class="collapse show" data-bs-parent="#accordion">
+                    <div class="card-body">
+                        Lorem ipsum..
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    <a class="collapsed btn" data-bs-toggle="collapse" href="#collapseFive">
+                        Gebäude E
+                    </a>
+                </div>
+                <div id="collapseFive" class="collapse" data-bs-parent="#accordion">
+                    <div class="card-body">
+                        Lorem ipsum..
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    <a class="collapsed btn" data-bs-toggle="collapse" href="#collapse6">
+                        Gebäude F
+                    </a>
+                </div>
+                <div id="collapse6" class="collapse" data-bs-parent="#accordion">
+                    <div class="card-body">
+                        Lorem ipsum..
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    <a class="btn" data-bs-toggle="collapse" href="#collapse7">
+                        Gebäude G
+                    </a>
+                </div>
+                <div id="collapse7" class="collapse show" data-bs-parent="#accordion">
+                    <div class="card-body">
+                        Lorem ipsum..
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    <a class="collapsed btn" data-bs-toggle="collapse" href="#collapse8">
+                        Gebäude H
+                    </a>
+                </div>
+                <div id="collapse8" class="collapse" data-bs-parent="#accordion">
+                    <div class="card-body">
+                        Lorem ipsum..
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    <a class="collapsed btn" data-bs-toggle="collapse" href="#collapse9">
+                        Gebäude W
+                    </a>
+                </div>
+                <div id="collapse9" class="collapse" data-bs-parent="#accordion">
+                    <div class="card-body">
+                        Lorem ipsum..
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    <a class="collapsed btn" data-bs-toggle="collapse" href="#collapse10">
+                        Home-Office
+                    </a>
+                </div>
+                <div id="collapse10" class="collapse" data-bs-parent="#accordion">
+                    <div class="card-body">
+                        Lorem ipsum..
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
 </div>
 <script type="text/javascript" src="../js/bootstrap.bundle.min.js"></script>
 </body>
