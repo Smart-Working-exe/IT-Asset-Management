8f7e6c0a3a9c08f7a5aa1ebcae3b5d55f699b322
mohamedzarrouk07 <mohamedaziz.zarrouk07@gmail.com>
2022-11-06 12:30:12 +0100

    Fixed: include von javascript
    gefixt

--- html/dashboard_studenten.php
+++ html/dashboard_studenten.php
@@ -10,7 +10,6 @@
           content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/styles/bootstrap.css">
     <link rel="stylesheet" href="/styles/custom.css">
-    <script src="js/bootstrap.bundle.min.js"></script>
     <title>Dashboard</title>
 </head>
 <body>
