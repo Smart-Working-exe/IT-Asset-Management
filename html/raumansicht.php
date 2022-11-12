9444c9d753fd70f7ab7870e01ea29e15e0a147ae
rihhii <jan.friedrich@alumni.fh-aachen.de>
2022-11-12 19:45:40 +0100

    Adds filter to Geräte Listen

--- html/raumansicht.php
+++ html/raumansicht.php
@@ -26,11 +26,6 @@
     </div>
     <div class="row mt-4 ">
         <p class="display-6 col-3"> Raum A102</p>
-        <div class="input-group mt-2 col-3" style="width: 30%; height: 2vh;">
-            <input type="search" class="form-control rounded" placeholder="Suche" aria-label="Search"
-                   aria-describedby="search-addon"/>
-            <button type="button" class="btn btn-primary sub"><img src="/img/search_icon.svg" width="30px"></button>
-        </div>
     </div>
     <div class="row"> <!-- Wegen Row ist die Progressbar abgeschnitten -->
         <p class="col-3"><b>IP-Adressbereich: 111.111.111.000/27</b></p>
@@ -55,6 +50,64 @@
             </button>
         </div>
     </div>
+
+
+    <form action="#" method="get">
+        <div class="row">
+
+            <div class="input-group mt-2 col-3" style="width: 20%; height: 2vh;">
+                <input type="search" class="form-control rounded" placeholder="Suche" aria-label="Search"
+                       aria-describedby="search-addon" name="search" id="search"/>
+            </div>
+
+            <div class="col mt-2">
+
+                <select class="form-select" name="typ" id="typ">
+                    <option value="none" selected>Typ</option>
+                    <option value="pC">PC</option>
+                    <option value="laptop">Laptop</option>
+                    <option value="sontiges">Sonstiges</option>
+                </select>
+
+            </div>
+
+            <div class="col mt-2">
+                <select class="form-select" name="hersteller" id="hersteller">
+                    <option value="none" selected>Hersteller</option>
+                    <option value="dell">Dell</option>
+                    <option value="hp">Hp</option>
+                </select>
+            </div>
+
+            <div class="col mt-2">
+                <input type="search" class="form-control rounded" placeholder="Alter" aria-label="Search"
+                       name="alter" id="alter">
+            </div>
+
+            <div class="col mt-2">
+                <select class="form-select" name="betriebssystem" id="betriebssystem">
+                    <option value="none" selected>Betriebssystem</option>
+                    <option value="windows">Windows</option>
+                    <option value="mac">Mac</option>
+                    <option value="linux">Linux</option>
+                </select>
+            </div>
+
+            <div class="col mt-2">
+                <select class="form-select" name="software" id="software">
+                    <option value="none" selected>Software</option>
+                    <option value="msoffice">MSOffice</option>
+                    <option value="visualstudio">Visual Studio</option>
+                    <option value="microchipstudio">Microchip Studio</option>
+                </select>
+            </div>
+
+            <div class="col mt-1">
+                <button type="submit" class="btn btn-primary sub"><img src="/img/search_icon.svg" width="30px"></button>
+            </div>
+
+        </div>
+    </form>
     <div class="container mt-3">
         <table class="table table-striped table-bordered" id="devices">
             <thead>
