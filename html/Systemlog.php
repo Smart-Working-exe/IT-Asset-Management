291fa8e1dd1e25ce5266a051d8b8f5ccd195841d
Riptash <martinthekiet8@web.de>
2022-11-13 03:15:50 +0100

    Update Systemlog.php

--- html/Systemlog.php
+++ html/Systemlog.php
@@ -7,9 +7,8 @@
     <link rel="stylesheet" href="/styles/bootstrap.css">
     <link rel="stylesheet" href="/styles/custom.css">
     <link rel="icon" type="image/x-icon" href="/img/fh-aachen_university-of-applied-sciences_303_logo.png">
-    <title>Systemlogs</title>
+    <title>Datenbank</title>
 </head>
-
 <body>
 <div class="container">
     <div class="row">
@@ -25,22 +24,74 @@
             <!-- </form> -->
         </div>
     </div>
-    <div><br> <br></div>
-    <fieldset class="border p-3">
-        <legend class="float-none w-auto" id="verschieben">Systemlogs</legend>
-        <div>
-            Jan 21: ma1234 hat sich abgemeldet. <br>
-            Jan 21: ma1234 hat die Raumbelegung in F111 angepasst. <br>
-            Jan 21: ma1234 hat sich angemeldet. <br>
-            Jan 18: cd5678 hat ein Gerät in F111 kommentiert.<br>
-        </div>
-    </fieldset>
-    <br>
-    <a href="dashboard_admin.php">
-        <button type="submit" class="btn btn-primary sub">Zurück zum Dashboard</button>
-    </a>
+
+    <div class="row mt-4 ">
+        <p class="display-6 col-3"> Systemlog</p>
+    </div>
+
+
+    <div class="container mt-3">
+        <table class="table table-striped table-bordered" id="log">
+            <thead>
+            <tr>
+                <th onclick="sortTable(0, log)">Benutzer <img src="/img/up-and-down-arrows-svgrepo-com.svg" width="20px"></th>
+                <th onclick="sortTable(1, log)">Benutzer <img src="/img/up-and-down-arrows-svgrepo-com.svg" width="20px"></th>
+                <th onclick="sortTable(2, log)">Aktion <img src="/img/up-and-down-arrows-svgrepo-com.svg" width="20px"></th>
+                <th onclick="sortTable(3, log)">Beschreibung <img src="/img/up-and-down-arrows-svgrepo-com.svg" width="20px"></th>
+
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>2021.12.13</td>
+                <td>mm5645s</td>
+                <td>Anmeldung</td>
+                <td>Der Benutzer mm5645s hat sich angemeldet</td>
+
+            </tr>
+
+            <tr>
+                <td>2013.02.14</td>
+                <td>mm8536m</td>
+                <td>Abmeldung</td>
+                <td>Der Benutzer mm8536m hat sich abgemeldet</td>
+
+            </tr>
+
+            <tr>
+                <td>2013.02.13</td>
+                <td>ls0420</td>
+                <td>Gerät löschen</td>
+                <td>Der Benutzer ls0420 hat ein Gerät im Raum A112 gelöscht</td>
+
+            </tr>
+
+            <tr>
+                <td>2022.02.12</td>
+                <td>cd5679</td>
+                <td>Gerät einfügen</td>
+                <td>Der Benutzer cd5679 hat ein Gerät im Raum A112 eingefügt</td>
+
+            </tr>
+
+            <tr>
+                <td>2022.07.27</td>
+                <td>fy2134</td>
+                <td>Ausleihe</td>
+                <td>Der Benutzer fy2134 hat eine Anfrage für die Ausleihe gesendet</td>
+
+            </tr>
+
+            </tbody>
+        </table>
+        <a href="dashboard_admin.php">
+            <button type="submit" class="btn btn-primary sub">Zurück zum Dashboard</button>
+        </a>
+    </div>
+
 </div>
-<footer>
+</body>
+<footer class="py-3 my-4 footerBot">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
         <li class="nav-item nav-link px-2 text-muted">Angemeldet als Benutzer ef8366m</li>
         <li class="nav-item"><a href="dashboard_admin.php" class="nav-link px-2 text-muted">Home</a></li>
@@ -49,7 +100,6 @@
     </ul>
     <p class="text-center text-muted">© 2022 SmartWorking.exe</p>
 </footer>
-
+<script type="text/javascript" src="../js/custom.js"></script>
 <script type="text/javascript" src="../js/bootstrap.bundle.min.js"></script>
-</body>
 </html>
