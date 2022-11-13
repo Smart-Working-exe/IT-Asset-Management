0e119e4c56a2b78888924c2ac79d623788f30e68
Riptash <martinthekiet8@web.de>
2022-11-13 15:17:53 +0100

    added filter

--- html/Systemlog.php
+++ html/Systemlog.php
@@ -28,13 +28,56 @@
     <div class="row mt-4 ">
         <p class="display-6 col-3"> Systemlog</p>
     </div>
+    <form action="#" method="get">
+        <div class="row">
 
+            <div class="input-group mt-2 col-3" style="width: 20%; height: 2vh;">
+                <input type="search" class="form-control rounded" placeholder="Suche" aria-label="Search"
+                       aria-describedby="search-addon" name="search" id="search"/>
+            </div>
+
+            <div class="col mt-2">
+                <input type="search" class="form-control rounded" placeholder="Datum" aria-label="Search"
+                       name="Datum" id="Datum">
+            </div>
+
+            <div class="col mt-2">
+                <input type="search" class="form-control rounded" placeholder="Benutzer" aria-label="Search"
+                       name="Benutzer" id="Benutzer">
+            </div>
+
+            <div class="col mt-2">
+
+                <select class="form-select" name="Aktion" id="Aktion">
+                    <option value="none" selected>Aktion</option>
+                    <option value="Abmeldung">Abmeldung</option>
+                    <option value="Anmeldung">Anmeldung</option>
+                    <option value="Ausleihe">Ausleihe</option>
+                    <option value="Gerät bearbeiten">Gerät bearbeiten</option>
+                    <option value="Gerät einfügen">Gerät einfügen</option>
+                    <option value="Gerät löschen">Gerät löschen</option>
+                </select>
+
+            </div>
+
+
+
+            <div class="col mt-2">
+                <input type="search" class="form-control rounded" placeholder="Description" aria-label="Search"
+                       name="Description" id="Description">
+            </div>
+            <div class="col mt-1">
+                <button type="submit" class="btn btn-primary sub"><img src="/img/search_icon.svg" width="30px"></button>
+            </div>
+
+        </div>
+    </form>
 
     <div class="container mt-3">
         <table class="table table-striped table-bordered" id="log">
             <thead>
             <tr>
-                <th onclick="sortTable(0, log)">Benutzer <img src="/img/up-and-down-arrows-svgrepo-com.svg" width="20px"></th>
+                <th onclick="sortTable(0, log)">Datum <img src="/img/up-and-down-arrows-svgrepo-com.svg" width="20px"></th>
                 <th onclick="sortTable(1, log)">Benutzer <img src="/img/up-and-down-arrows-svgrepo-com.svg" width="20px"></th>
                 <th onclick="sortTable(2, log)">Aktion <img src="/img/up-and-down-arrows-svgrepo-com.svg" width="20px"></th>
                 <th onclick="sortTable(3, log)">Beschreibung <img src="/img/up-and-down-arrows-svgrepo-com.svg" width="20px"></th>
