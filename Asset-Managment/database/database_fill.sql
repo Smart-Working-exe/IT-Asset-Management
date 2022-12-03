1be282dcd7c2274f52457093f09de3d2cea3c9c9
rihhii <jan.friedrich@alumni.fh-aachen.de>
2022-12-03 17:03:30 +0100

    update database_fill

--- Asset-Managment/database/database_fill.sql
+++ Asset-Managment/database/database_fill.sql
@@ -8,22 +8,22 @@ INSERT INTO personen (vorname, nachname, fh_kuerzel, rolle, passwort) VALUES
     ('Thomas','Wagner','tw0850m',2,'springenderTitan');
 
 INSERT INTO geraet (name, Typ, hersteller, betrieb, ausleihbar, raumnummer, `ip-adresse`, technische_eckdaten, kommentar) VALUES
-                                                                                                                              ('PC-1','Tower-PC','Dell','20-11-30',0,'a001','156.0.0.1','USB 3.0; HDMI',''),
-                                                                                                                              ('PC-2','Tower-PC','Dell','20-11-30',0,'a001','156.0.0.2','USB 3.0; HDMI',''),
-                                                                                                                              ('PC-3','Tower-PC','Dell','20-11-30',0,'a002','156.0.1.1','USB 3.0; HDMI','Der Kommentar'),
-                                                                                                                              ('PC-4','Tower-PC','Dell','20-11-30',0,'a002','156.0.1.2','USB 3.0; HDMI',''),
+                                                                                                                              ('PC-1','PC','Dell','20-11-30',0,'a001','156.0.0.1','USB 3.0; HDMI',''),
+                                                                                                                              ('PC-2','PC','Dell','20-11-30',0,'a001','156.0.0.2','USB 3.0; HDMI',''),
+                                                                                                                              ('PC-3','PC','Dell','20-11-30',0,'a002','156.0.1.1','USB 3.0; HDMI','Der Kommentar'),
+                                                                                                                              ('PC-4','PC','Dell','20-11-30',0,'a002','156.0.1.2','USB 3.0; HDMI',''),
                                                                                                                               ('Monitor-1','Monitor','Dell','20-12-1',0,'a001','','24 Zoll;Display Port; HDMI',''),
                                                                                                                               ('Monitor-2','Monitor','Dell','20-11-30',0,'a001','','24 Zoll;Display Port; HDMI','Pixelfehler in der Mitte'),
                                                                                                                               ('Monitor-3','Monitor','Dell','20-12-1',0,'a002','','24 Zoll;Display Port; HDMI',''),
                                                                                                                               ('Monitor-4','Monitor','Dell','20-11-30',0,'a002','','24 Zoll;Display Port; HDMI',''),
-                                                                                                                              ('Maus-1','PC-Maus','Corsair','20-12-1',0,'a001','','1000 mhz',''),
-                                                                                                                              ('Maus-2','PC-Maus','Corsair','20-11-30',0,'a001','','1000 mhz','Mausrad defekt'),
-                                                                                                                              ('Maus-3','PC-Maus','Corsair','20-12-1',0,'a002','','1000 mhz',''),
-                                                                                                                              ('Maus-4','PC-Maus','Corsair','20-11-30',0,'a002','','1000 mhz',''),
-                                                                                                                              ('Tastatur-1','PC-Tastatur','Logitech','20-12-1',0,'a001','','',''),
-                                                                                                                              ('Tastatur-2','PC-Tastatur','Logitech','20-11-30',0,'a001','','',''),
-                                                                                                                              ('Tastatur-3','PC-Tastatur','Logitech','20-12-1',0,'a002','','',''),
-                                                                                                                              ('Tastatur-4','PC-Tastatur','Logitech','20-11-30',0,'a002','','',' Numpad funktioniert nicht');
+                                                                                                                              ('Maus-1','Maus','Corsair','20-12-1',0,'a001','','1000 mhz',''),
+                                                                                                                              ('Maus-2','Maus','Corsair','20-11-30',0,'a001','','1000 mhz','Mausrad defekt'),
+                                                                                                                              ('Maus-3','Maus','Corsair','20-12-1',0,'a002','','1000 mhz',''),
+                                                                                                                              ('Maus-4','Maus','Corsair','20-11-30',0,'a002','','1000 mhz',''),
+                                                                                                                              ('Tastatur-1','Tastatur','Logitech','20-12-1',0,'a001','','',''),
+                                                                                                                              ('Tastatur-2','Tastatur','Logitech','20-11-30',0,'a001','','',''),
+                                                                                                                              ('Tastatur-3','Tastatur','Logitech','20-12-1',0,'a002','','',''),
+                                                                                                                              ('Tastatur-4','Tastatur','Logitech','20-11-30',0,'a002','','',' Numpad funktioniert nicht');
 
 INSERT INTO geraet( name, Typ, hersteller, betrieb, personen_id, technische_eckdaten, kommentar) VALUES
     ('Laptop-1','Laptop','Lenovo','220-08-05','tw0850m','14 Zoll; HDMI','');
